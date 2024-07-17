58a01bfe236b560479293972fab9923044294ad6
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-07-17 13:27:50 -0300

    visuals

--- app/les_testes__/page.tsx
+++ app/les_testes__/page.tsx
@@ -7,14 +7,19 @@ const Teste = () => {
 
   return (
     <div>
-      <h1>Página de testes</h1>
-      <form action="triggerSubscribeEmail">
-        <input type="text" name="subscriberName" id="subscriberName" placeholder="nome"/>
-        <input type="text" name="subscriberEmail" id="subscriberEmail" placeholder="email"/>
-        <button type="button">Send E-mail</button>
-      </form>
+      <h1 className='py-6'>Página de testes</h1>
+      <section className='p-4 bg-[#fff9] shadow-md '>
+        <h2>Envio de E-mail</h2>
+        <form action="triggerSubscribeEmail">
+          <div className='flex flex-col gap-2'>
+            <input type="text" name="subscriberName" id="subscriberName" placeholder="nome" />
+            <input type="text" name="subscriberEmail" id="subscriberEmail" placeholder="email" />
+            <button type="button" className='btn ok ml-auto'>Send E-mail</button>
+          </div>
+        </form>
 
-    </div>
+      </section>
+    </div >
   );
 };
 
