46d1e1ae652fe5b29fdb83d9ed9a2a9d3e106cdb
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-07-17 12:56:48 -0300

    add page for executing tests

--- app/_testes/page.tsx
+++ app/_testes/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import triggerSubscribeEmail from "@/netlify/functions/triggerSubscribeEmail";
+
 const Teste = () => {
+
 
   return (
     <div>
       <h1>Página de testes</h1>
-      
+      <form action="triggerSubscribeEmail">
+        <input type="text" name="subscriberName" id="subscriberName" placeholder="nome"/>
+        <input type="text" name="subscriberEmail" id="subscriberEmail" placeholder="email"/>
+        <button type="button">Send E-mail</button>
+      </form>
+
     </div>
   );
 };
