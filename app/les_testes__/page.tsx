efc4eb9906a382faf1262afaa915b43efdb60b13
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-07-30 13:57:11 -0300

    teste email

--- app/les_testes__/page.tsx
+++ app/les_testes__/page.tsx
@@ -1,8 +1,31 @@
-"use client";
+"use client"
 
-import triggerSubscribeEmail from "@/netlify/functions/triggerSubscribeEmail";
+import { useRef } from "react";
 
 const Teste = () => {
+
+  const mailForm = useRef(null);
+
+  const sendEmail = async () => {
+    if (!mailForm.current) {
+      console.log('Form not found');
+      return
+    };
+    const formData = new FormData(mailForm.current);
+    const name = formData.get('name') as string;
+    const email = formData.get('email') as string;
+    const message = formData.get('message') as string;
+
+    const response = await fetch('/.netlify/functions/triggerContactFormMessage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, message }),
+    });
+
+    console.log(response);
+  }
 
 
   return (
@@ -10,11 +33,13 @@ const Teste = () => {
       <h1 className='py-6'>Página de testes</h1>
       <section className='p-4 bg-[#fff9] shadow-md '>
         <h2>Envio de E-mail</h2>
-        <form action="triggerSubscribeEmail">
+        {/* @ts-ignore */}
+        <form ref={mailForm} netlify data-netlify="true">
           <div className='flex flex-col gap-2'>
-            <input type="text" name="subscriberName" id="subscriberName" placeholder="nome" />
-            <input type="text" name="subscriberEmail" id="subscriberEmail" placeholder="email" />
-            <button type="button" className='btn ok ml-auto'>Send E-mail</button>
+            <input type="text" name="name" id="name" placeholder="nome" />
+            <input type="text" name="email" id="email" placeholder="email" />
+            <textarea name="message" id="message" placeholder="mensagem" cols={30} rows={10}></textarea>
+            <button type="button" className='btn ok ml-auto' onClick={sendEmail}>Send E-mail</button>
           </div>
         </form>
 
